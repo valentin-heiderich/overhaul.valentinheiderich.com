964e113a866fab199aa0b56f43a1ce47d844f34e
Valentin Heiderich <valentin.heiderich@gmx.de>
2024-05-26 00:48:23 +0200

    Initial commit

---
+++ src/main/java/overhaul/valentinheiderich/com/ValentinsOverhaulClient.java
@@ -0,0 +1,10 @@
+package overhaul.valentinheiderich.com;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ValentinsOverhaulClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
